265d2023818191222faec8d344b885dd6d65d927
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2022-04-23 16:48:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+video = "";
+status1 = "";
+name1 = "";
 function setup() {
     canvas = createCanvas(730, 430);
     canvas.center();
+    video = createVideo("video.mp4");
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 730, 430);
+}
+function start() {
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    name1 = document.getElementById("input_name").value;
+}
+function modelLoaded() {
+    console.log("model is loaded!");
+    status1 = true;
 }
